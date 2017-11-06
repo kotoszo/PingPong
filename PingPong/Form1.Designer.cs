318aec520e615176adc7b6c9739ba8341b68f6e6
Unknown <rtomilobil@gmail.com>
2017-11-06 07:46:02 +0100

    kinda done

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace PingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.question = new System.Windows.Forms.Label();
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.question);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(804, 419);
+            this.playground.TabIndex = 2;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(402, 122);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(21, 18);
+            this.ball.TabIndex = 3;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.racket.Location = new System.Drawing.Point(245, 279);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(315, 17);
+            this.racket.TabIndex = 2;
+            this.racket.TabStop = false;
+            // 
+            // question
+            // 
+            this.question.AutoSize = true;
+            this.question.Font = new System.Drawing.Font("Palatino Linotype", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.question.Location = new System.Drawing.Point(395, 202);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(381, 161);
+            this.question.TabIndex = 4;
+            this.question.Text = "label1";
+            this.question.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 419);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Label question;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        bool? isPlaying;
+
+        private const int basicSpeed = 4;
+
+        private int movementSpeed = 15;
+        private int speed_left = basicSpeed;
+        private int speed_top = basicSpeed;
+        private int point = 0;
+        private bool isLeftPressed, isRightPressed;
+
+        Stream introMusic = Properties.Resources.introMusic;
+        Stream gameplayMusic = Properties.Resources.gameplayMusic;
+        private SoundPlayer music;
+        private Random random = new Random();
+        int bpmCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+            Bounds = Screen.PrimaryScreen.Bounds;
+
+            racket.Top = playground.Bottom - (playground.Bottom / 10);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool? moveLeft = null;
+            bpmCounter++;
+            if (bpmCounter == 40)
+            {
+                playground.BackColor = Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                bpmCounter = 0;
+            }
+
+            if (isLeftPressed) { moveLeft = true; }
+            if (isRightPressed)
+            {
+                if (moveLeft.HasValue)
+                {
+                    moveLeft = null;
+                }
+                else
+                {
+                    moveLeft = false;
+                }
+            }
+            PlayerMove(moveLeft);
+            BallMove();
+
+            if (ball.Bounds.IntersectsWith(racket.Bounds))
+            {
+                BouncingBall();
+            }
+            if (ball.Left <= playground.Left || ball.Right >= playground.Right) speed_left = -speed_left;
+            if (ball.Top <= playground.Top) speed_top = -speed_top;
+            if (ball.Bottom >= playground.Bottom) { GameOver(); }
+        }
+
+        private void BallMove()
+        {
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+        }
+
+        private void BouncingBall()
+        {
+            speed_top += 2;
+            speed_left += 2;
+            speed_top = -speed_top;
+            point += 1;
+        }
+        private void PlayerMove(bool? moveLeft)
+        {
+            if (racket.Right >= playground.Right) isRightPressed = false;
+            if (racket.Left <= playground.Left) isLeftPressed = false;
+            if (isLeftPressed || isRightPressed)
+            {
+                if (moveLeft.HasValue)
+                {
+                    var speed = movementSpeed;
+                    if (moveLeft.Value)
+                    {
+                        speed *= -1;
+                    }
+                    racket.Location = new Point(racket.Location.X + speed, racket.Location.Y);
+                }
+            }
+        }
+        private void GameOver()
+        {
+            timer1.Enabled = false;
+            question.Text = "Wanna play more?\nY/N";
+            isPlaying = false;
+            question.Show();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            music = new SoundPlayer(introMusic);
+            music.PlayLooping();
+            question.Text = "Are you ready?\n Y/N";
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { isLeftPressed = false; }
+            if (e.KeyCode == Keys.Right) { isRightPressed = false; }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    Application.Exit();
+                    break;
+                case Keys.A:
+                case Keys.Left:
+                    isLeftPressed = true;
+                    break;
+                case Keys.D:
+                case Keys.Right:
+                    isRightPressed = true;
+                    break;
+                case Keys.Y:
+                    WhatWithY();
+                    break;
+                case Keys.N:
+                    if (!isPlaying.Value) { Application.Exit(); }
+                    break;
+            }
+        }
+
+        private void WhatWithY()
+        {
+            if (isPlaying.HasValue)
+            {
+                if (!isPlaying.Value)
+                {
+                    StartGame(false);
+                }
+            }
+            else { StartGame(true); }
+        }
+
+        private void StartGame(bool firstTime)
+        {
+            if (firstTime)
+            {
+                question.Hide(); timer1.Enabled = true; music = new SoundPlayer(gameplayMusic); music.PlayLooping();
+                isPlaying = true;
+            } else
+            {
+                ball.Location = new Point(playground.Width / 2, playground.Height / 4);
+                timer1.Enabled = true;
+                question.Hide();
+            }
+            speed_top = basicSpeed;
+            speed_left = basicSpeed;
+        }
+    }
+}
